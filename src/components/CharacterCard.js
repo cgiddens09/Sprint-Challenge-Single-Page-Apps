f330e2d210b57a58e2c0c4211878bb15658f73a2
Christina Giddens <giddens.christina@gmail.com>
2020-03-22 22:02:38 -0400

    Not completed

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return(
+    <div>
+      <img src={props.image} alt='character image' />
+      <h2>Name: {props.name}</h2>
+      <p>Gender: {props.gender}</p>
+      <p>Species: {props.species}</p>
+      <p>Status: {props.status}</p>
+
+    </div>
+  )
 }
